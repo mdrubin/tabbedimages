c4a4cad53df0415372edab3587937afbdca7f6c2
andrzejkrzywda <andrzejkrzywda@49f89456-d227-0410-8bee-f9589126b591>
2007-02-11 01:09:51 +0000

    Extracted createTab(filename) method.

--- trunk/main.py
+++ trunk/main.py
@@ -59,7 +59,32 @@ class MainForm(Form):
         
         self.Controls.Add(menuStrip)
             
-    
+            
+    def createTab(self, fileName):
+        image = None
+        try:
+            image = Bitmap(fileName)
+        except ArgumentException:
+            MessageBox.Show(fileName + " doesnt't appear to be a valid image file", 
+                            "Invalid image format",
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            return
+        
+        tabPage = self.tabControl.TabPages[0]
+        if tabPage.Text != NO_IMAGE:
+            tabPage = TabPage()
+            self.tabControl.TabPages.Add(tabPage)
+            self.tabControl.SelectedTab = tabPage
+        tabPage.Text = Path.GetFileName(fileName)
+        pictureBox = PictureBox(
+            Dock = DockStyle.Fill,
+            Image = image,                
+            SizeMode = PictureBoxSizeMode.StretchImage
+        )
+        tabPage.Controls.Clear()
+        tabPage.Controls.Add(pictureBox)        
+            
+            
     def onOpen(self, _, __):
         openFileDialog = OpenFileDialog(
             Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*",
@@ -68,28 +93,7 @@ class MainForm(Form):
         if openFileDialog.ShowDialog() == DialogResult.Cancel:
             return
         for fileName in openFileDialog.FileNames:
-
-            image = None
-            try:
-                image = Bitmap(fileName)
-            except ArgumentException:
-                MessageBox.Show(fileName + " doesnt't appear to be a valid image file", 
-                                "Invalid image format",
-                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
-            if image:
-                tabPage = self.tabControl.TabPages[0]
-                if tabPage.Text != NO_IMAGE:
-                    tabPage = TabPage()
-                    self.tabControl.TabPages.Add(tabPage)
-                    self.tabControl.SelectedTab = tabPage
-                tabPage.Text = Path.GetFileName(fileName)
-                pictureBox = PictureBox(
-                    Dock = DockStyle.Fill,
-                    Image = image,                
-                    SizeMode = PictureBoxSizeMode.StretchImage
-                )
-                tabPage.Controls.Clear()
-                tabPage.Controls.Add(pictureBox)
+            self.createTab(fileName)
         
 
 Application.EnableVisualStyles()
