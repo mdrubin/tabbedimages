9996fe83ba6b88bb6f8cfca89a45819acdc993b8
fuzzyman <fuzzyman@49f89456-d227-0410-8bee-f9589126b591>
2007-02-19 15:59:15 +0000

    Further clipboard handling.

--- trunk/Clipboard/Clipboard.cs
+++ trunk/Clipboard/Clipboard.cs
@@ -9,5 +9,14 @@ namespace Clipboard
     {
         [DllImport("User32.dll", CharSet = CharSet.Auto)]
         public static extern IntPtr SetClipboardViewer(IntPtr hWndNewViewer);
+
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool
+               ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg,
+                                             IntPtr wParam,
+                                             IntPtr lParam);
     }
 }
